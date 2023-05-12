7e71b3de3ffcc092324a4c24d148b67d9b27d67a
c30net <c30net@gmail.com>
2023-05-12 21:56:38 +0330

    make a simple game to practicealittle js

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 This is a game without point about earning money
 a commentary on the pursuit of wealth and never being fulfilled
+
+-   pictures width and height that calculates and gets by javascript seems to be sometimes inaccurate. I don't know the rules of them.
+    the best way that seems to me be reasonable is to write them as width and height attributes for img tag.
+
+*   the function that has been written for overlapping is based on when 2 elements are not overlapping and then making it negative.  
+    I wrote another function as overlapping 2 but I wasn't sure is it working totally fine or not, because in stakoverflow they wrote it as the first.
+
+*   window.getComputedStyle(elementObject) measures sizes differently from elementObject.getBoundingClientRect(), getComputedStyle measures for right, from right side of element to right side of browser, but, getBoundingClientRect calulate for right, from right side of element to left side of browser
+*   after changing the position of an element in the page , numbers that getBoundingClientRect returns to us doesn't change and it's not good but get computed style measures changes when position changes and it's good and makes it more reliable or maybe something I'm doing wrong and I don't know yet
+*   after giving value with elementObject.style we can readthem for increase or decreaase after event listener is triggered
+
+*   The way we think about an app at the start isn't the same as the app works at the end. It means that at the end we may delete some codes that we needed to understand the problem and start to solve the problem but when work goes on our solution and our problem changes. at the start may I need a variable to tell where the position should be but at the end I don't need that because after loading the page with random function the position of every element will be made.
--- app.js
+++ app.js
@@ -1,53 +1,58 @@
 let coin = document.querySelector('#coin');
 let coinRect = window.getComputedStyle(coin);
+
 let runner = document.querySelector('#runner');
 let runnerRect = window.getComputedStyle(runner);
-let runnerCurrentTop = parseInt(
-    window.getComputedStyle(runner).top.slice(0, -2)
-);
-let runnerCurrentLeft = parseInt(
-    window.getComputedStyle(runner).left.slice(0, -2)
-);
-let changedRunnerTop = runnerCurrentTop + 20;
-let changedRunnerLeft = runnerCurrentLeft + 20;
+//read current position with element.style.top and element.style.left
+// and store it plus or minus 20 for every stroke on 4 arrow keys
+let changedRunnerTop;
+let changedRunnerLeft;
+//when page loads change the position of coin and runner if they are overlapping
+window.addEventListener('DOMContentLoaded', function () {
+    console.log('isoverlapping : ' + isOverlaping());
+    console.log('isoverlapping2 : ' + isOverlapping2());
+    if (isOverlaping()) {
+        makeRandomPlace(runner);
+        makeRandomPlace(coin);
+    }
+});
+
+const makeRandomPlace = function (elm) {
+    let x = Math.floor(Math.random() * (window.innerWidth - 100));
+    let y = Math.floor(Math.random() * (window.innerHeight - 100));
+    elm.style.top = `${y}px`;
+    elm.style.left = `${x}px`;
+};
 
 window.addEventListener('keydown', (e) => {
     if (e.key === 'ArrowDown') {
-        changedRunnerTop += 20;
+        changedRunnerTop = pos(runner, 'top') + 20;
         runner.style.setProperty('top', `${changedRunnerTop}px`);
     } else if (e.key === 'ArrowUp') {
-        changedRunnerTop -= 20;
+        changedRunnerTop = pos(runner, 'top') - 20;
         runner.style.setProperty('top', `${changedRunnerTop}px`);
     } else if (e.key === 'ArrowLeft') {
-        changedRunnerLeft -= 20;
+        changedRunnerLeft = pos(runner, 'left') - 20;
+        runner.style.transform = 'scale(-1,1)';
         runner.style.setProperty('left', `${changedRunnerLeft}px`);
     } else if (e.key === 'ArrowRight') {
-        changedRunnerLeft += 20;
+        changedRunnerLeft = pos(runner, 'left') + 20;
+        runner.style.transform = 'scale(1,1)';
         runner.style.setProperty('left', `${changedRunnerLeft}px`);
     }
+    //listen to event of keys if images were overlapping change their positions
 
-    console.log(isOverlaping());
+    if (isOverlaping()) {
+        makeRandomPlace(runner);
+        makeRandomPlace(coin);
+    }
 });
+const pos = function (elem, direct) {
+    return parseInt(elem.style[direct].slice(0, -2));
+};
 
-function isOverlaping() {
-    console.log(
-        'parseInt(coinRect.left.slice(0, -2)) >  parseInt(runnerRect.right.slice(0, -2))',
-        parseInt(coinRect.left.slice(0, -2)),
-        parseInt(runnerRect.left.slice(0, -2)) +
-            parseInt(runnerRect.width.slice(0, -2)),
-        'parseInt(coinRect.bottom.slice(0, -2)) <  arseInt(runnerRect.top.slice(0, -2))',
-        parseInt(coinRect.top.slice(0, -2)) +
-            parseInt(coinRect.height.slice(0, -2)),
-        parseInt(runnerRect.top.slice(0, -2)),
-        'parseInt(coinRect.right.slice(0, -2)) <  parseInt(runnerRect.left.slice(0, -2))',
-        parseInt(coinRect.left.slice(0, -2)) +
-            parseInt(coinRect.width.slice(0, -2)),
-        parseInt(runnerRect.left.slice(0, -2)),
-        'parseInt(coinRect.top.slice(0, -2)) >  parseInt(runnerRect.bottom.slice(0, -2))',
-        parseInt(coinRect.top.slice(0, -2)),
-        parseInt(runnerRect.top.slice(0, -2)) +
-            parseInt(runnerRect.height.slice(0, -2))
-    );
+//check if images are overlapping
+const isOverlaping = function () {
     if (
         !(
             parseInt(coinRect.left.slice(0, -2)) >
@@ -68,7 +73,25 @@ function isOverlaping() {
     } else {
         return false;
     }
-}
+};
 
-let x = Math.floor(Math.random() * (window.innerWidth - 100));
-let y = Math.floor(Math.random() * (window.innerHeight - 100));
+const isOverlapping2 = function () {
+    if (
+        parseInt(coinRect.left.slice(0, -2)) <=
+            parseInt(runnerRect.left.slice(0, -2)) +
+                parseInt(runnerRect.width.slice(0, -2)) &&
+        parseInt(coinRect.top.slice(0, -2)) +
+            parseInt(coinRect.height.slice(0, -2)) >=
+            parseInt(runnerRect.top.slice(0, -2)) &&
+        parseInt(runnerRect.left.slice(0, -2)) <=
+            parseInt(coinRect.left.slice(0, -2)) +
+                parseInt(coinRect.width.slice(0, -2)) &&
+        parseInt(runnerRect.top.slice(0, -2)) +
+            parseInt(runnerRect.height.slice(0, -2)) >=
+            parseInt(coinRect.top.slice(0, -2))
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+};
